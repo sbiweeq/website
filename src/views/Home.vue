88b9f48ed7aecd4bdf897b3311f1f74133027a81
李宇翔 <178529795@qq.com>
2020-03-05 10:26:53 +0800

    修复start按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,78 @@
 <template>
-  <div>
-    
-    <img src="../assets/logo.png">
     <div>
-      <p>
-        Monibuca 是一个开源的Go语言实现的流媒体服务器开发框架
-      </p>
-       <div style="margin-bottom:10px">Monibuca 引擎 + 插件 = 定制化流媒体服务器</div>
-      <Button type="success" to="/docs" target="_blank">🚀START</Button>
-      <span style="margin: 0 10px"></span>
-      <Button type="default" target="_blank" to="https://github.com/Monibuca">
-        <svg style="vertical-align: text-top" width="16" height="16" aria-labelledby="simpleicons-github-dark-icon" lang="" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title id="simpleicons-github-dark-icon" lang="en">GitHub Dark icon</title><path fill="#7F8C8D" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg>
-        GITHUB</Button>
-    </div>
-    <Row style="margin: 30px;">
-      <Col span="6">
-        <Card :bordered="false" style="margin: 30px">
-          <div slot="title" class="feature-title">⚡高性能 低功耗</div>
-          <div>针对流媒体服务器独特的性质进行的优化，充分利用Golang的goroutine的性质对大量的连接的读写进行合理的分配计算资源，以及尽可能的减少内存Copy操作。使用对象池减少Golang的GC时间。</div>
-        </Card>
-      </Col>
-      <Col span="6">
-        <Card :bordered="false" style="margin: 30px">
-          <div slot="title" class="feature-title">🔧模块化 可扩展</div>
-          <div>专为二次开发而设计，基于Golang语言，开发效率更高；独创的插件机制，可以方便用户定制个性化的功能组合，更高效率的利用服务器资源。</div>
-        </Card>
-      </Col>
-      <Col span="6">
-        <Card :bordered="false" style="margin: 30px">
-          <div slot="title" class="feature-title">📈可视化 易管理</div>
-          <div>功能强大的仪表盘可以直观的看到服务器运行的状态、消耗的资源、以及其他统计信息。用户可以利用控制台对服务器进行配置和控制。点击右上角菜单栏里面的演示，可以看到演示控制台界面。</div>
-        </Card>
-      </Col>
-      <Col span="6">
-        <Card :bordered="false" style="margin: 30px">
-          <div slot="title" class="feature-title">🐹零依赖 易部署</div>
-          <div>纯Go编写，不依赖cgo，不依赖FFMpeg或者其他运行时，部署极其方便，对服务器的要求极为宽松。</div>
-        </Card>
-      </Col>
-    </Row>
-    <div id="content">
+        <img src="../assets/logo.png" />
+        <div>
+            <p>Monibuca 是一个开源的Go语言实现的流媒体服务器开发框架</p>
+            <div style="margin-bottom:10px">Monibuca 引擎 + 插件 = 定制化流媒体服务器</div>
+            <Button type="success" to="http://docs.monibuca.com" target="_blank">🚀START</Button>
+            <span style="margin: 0 10px"></span>
+            <Button type="default" target="_blank" to="https://github.com/Monibuca">
+                <svg
+                    style="vertical-align: text-top"
+                    width="16"
+                    height="16"
+                    aria-labelledby="simpleicons-github-dark-icon"
+                    lang
+                    role="img"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <title id="simpleicons-github-dark-icon" lang="en">GitHub Dark icon</title>
+                    <path
+                        fill="#7F8C8D"
+                        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                    />
+                </svg>
+                GITHUB
+            </Button>
+        </div>
+        <Row style="margin: 30px;">
+            <Col span="6">
+                <Card :bordered="false" style="margin: 30px">
+                    <div slot="title" class="feature-title">⚡高性能 低功耗</div>
+                    <div>针对流媒体服务器独特的性质进行的优化，充分利用Golang的goroutine的性质对大量的连接的读写进行合理的分配计算资源，以及尽可能的减少内存Copy操作。使用对象池减少Golang的GC时间。</div>
+                </Card>
+            </Col>
+            <Col span="6">
+                <Card :bordered="false" style="margin: 30px">
+                    <div slot="title" class="feature-title">🔧模块化 可扩展</div>
+                    <div>专为二次开发而设计，基于Golang语言，开发效率更高；独创的插件机制，可以方便用户定制个性化的功能组合，更高效率的利用服务器资源。</div>
+                </Card>
+            </Col>
+            <Col span="6">
+                <Card :bordered="false" style="margin: 30px">
+                    <div slot="title" class="feature-title">📈可视化 易管理</div>
+                    <div>功能强大的仪表盘可以直观的看到服务器运行的状态、消耗的资源、以及其他统计信息。用户可以利用控制台对服务器进行配置和控制。点击右上角菜单栏里面的演示，可以看到演示控制台界面。</div>
+                </Card>
+            </Col>
+            <Col span="6">
+                <Card :bordered="false" style="margin: 30px">
+                    <div slot="title" class="feature-title">🐹零依赖 易部署</div>
+                    <div>纯Go编写，不依赖cgo，不依赖FFMpeg或者其他运行时，部署极其方便，对服务器的要求极为宽松。</div>
+                </Card>
+            </Col>
+        </Row>
+        <div id="content">
             <div :id="'shadow'+(16-n)" v-for="n in 15" :key="n" class="shadow"></div>
             <div :id="'gear'+(16-m)" v-for="m in 15" :key="m+16"></div>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
-
 </script>
 <style scoped>
-@import url('../assets/zzsc.css');
-  .feature-title{
+@import url("../assets/zzsc.css");
+.feature-title {
     color: #eb5e46;
     font-weight: bold;
     font-size: larger;
-  }
-  p {
+}
+p {
     margin: 30px;
     font-size: 20px;
-  }
-  img{
+}
+img {
     margin: 20px;
-  }
+}
 </style>
