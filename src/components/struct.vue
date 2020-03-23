c6beb3a9a8685c99c3dd255cac62fd1ec3ac45a6
李宇翔 <178529795@qq.com>
2020-03-23 18:39:58 +0800

    动态生成图像

--- src/components/struct.vue
+++ src/components/struct.vue
@@ -10,127 +10,31 @@
         </g>
         <g>
             <title>Layer 1</title>
+            <template v-for="(publisher,i) in publishers">
+                <template v-for="(p,index) in publisher">
+                    <rect fill="#fff" :key="index+'r'+i" stroke="#000" :y="i*lineheight+yOffset"
+                        :x="getPublisherAfterWidth(i,index)" height="33" :width="p.bbox.width+20" />
+                    <text :ref="index+'t'+i" stroke-width="0" :key="index+'t'+i" :x="getPublisherAfterWidth(i,index)+10"
+                        :y="i*lineheight+20+yOffset" fill="#000000" stroke="#000">{{p.label}}</text>
+                    <line :key="index+'l'+i" stroke="#000" stroke-width="7"
+                        :x1="getPublisherAfterWidth(i,index)+p.bbox.width+20" :y1="i*lineheight+yOffset+15"
+                        :x2="getPublisherAfterWidth(i,index)+p.bbox.width+50" :y2="i*lineheight+yOffset+15"
+                        v-if="index!=publisher.length-1" />
+                    <path stroke-dasharray='10, 10, 5' stroke="black" fill="#fff" stroke-width="7" :d="getPath(i)"
+                        :key="index+'p'+i">
+                        <animate attributeName="stroke-dashoffset" values="50;0" dur="2s" repeatCount="indefinite" />
+                    </path>
+                </template>
+            </template>
             <ellipse ry="49.5" rx="49.5" id="svg_2" cy="200" cx="500" stroke-width="1.5" stroke="#000" fill="#fff" />
             <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24"
                 id="svg_3" y="206.45313" x="464" stroke-width="0" stroke="#000" fill="#000000">engine</text>
-            <rect id="svg_4" height="34" width="117" y="100.45313" x="267" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <path stroke="#000" id="svg_5"
-                d="m682.62935,352.46299c-2.22495,-2.19747 -4.62467,-3.95778 -6.59319,-4.19777c-0.10716,-0.01557 -0.21816,-0.02465 -0.33553,-0.02724l-12.95546,0c-0.20668,0 -0.4108,0.08172 -0.55624,0.2335c-0.14416,0.14788 -0.22964,0.35544 -0.22964,0.56688l0,33.40313c0,0.21015 0.08037,0.41122 0.22964,0.56429c0.14927,0.15048 0.34573,0.2322 0.55624,0.2322l23.42827,0c0.20668,0 0.40442,-0.08043 0.55368,-0.2322c0.14927,-0.15177 0.23219,-0.35284 0.23219,-0.56429l0,-22.22768c-0.04593,-2.58015 -2.03358,-5.3938 -4.32997,-7.75082zm-1.10609,1.13117c1.027,1.01961 1.95065,2.17412 2.63957,3.28713c-0.59706,-0.23998 -1.22347,-0.4177 -1.8116,-0.5591c-1.29491,-0.29576 -2.56175,-0.41511 -3.3489,-0.4631c0.00383,-0.17253 0.0051,-0.36711 0.0051,-0.57856c0,-1.12727 -0.07017,-2.73452 -0.45417,-4.15107c-0.00383,-0.01297 -0.00638,-0.02594 -0.01148,-0.03892c0.97469,0.65379 2.03613,1.52163 2.98148,2.50361zm3.86176,28.04436l-21.85652,0l0,-31.80497l12.17341,0l0,0.00259c0.53072,-0.03243 0.98872,0.50072 1.33956,1.7201c0.31894,1.161 0.39932,2.67485 0.39804,3.72429c0.00255,0.76924 -0.03445,1.28553 -0.03445,1.28553l-0.06251,0.84578l0.83691,0.00908c0.00383,0 1.93407,0.02335 3.82732,0.46051c1.81925,0.39954 3.24174,1.19603 3.37442,2.09759c0.0051,0.08043 0.00638,0.16085 0.0051,0.23609l0,21.42341l-0.00128,0z"
-                fill="#fff" />
             <path id="svg_6"
                 d="m504.25827,251.88514c-1.44512,0.136 -2.92259,0.23002 -4.44215,0.23002l0,0c-1.35921,0 -2.67955,-0.07442 -3.96753,-0.18475l0,0l-11.15224,14.33405l-2.74267,-0.77754c-3.21901,-0.90395 -6.34573,-2.03802 -9.35247,-3.39882l0,0l-2.60659,-1.17938l0.67067,-18.005c-2.37013,-1.65899 -4.58141,-3.50582 -6.62925,-5.53075l0,0l-17.63238,3.78768l-1.61031,-2.36545c-1.88896,-2.77664 -3.49927,-5.6863 -4.92645,-8.62811l0,0l-1.23936,-2.56949l11.97204,-13.25179c-0.7485,-2.80596 -1.24419,-5.70262 -1.50016,-8.65414l0,0l-15.86496,-8.38845l0.28835,-2.84166c0.34026,-3.39889 0.99793,-6.65182 1.78365,-9.77846l0,0l0.70152,-2.77361l17.69557,-2.44945c1.26197,-2.66334 2.74761,-5.18732 4.42105,-7.55911l0,0l-6.75226,-16.6247l2.03802,-1.99589c2.39595,-2.33286 4.96052,-4.44857 7.63675,-6.38294l0,0l2.31828,-1.67176l15.27527,9.59371c2.57429,-1.18579 5.26674,-2.16107 8.05642,-2.90959l0,0l5.53728,-17.18201l2.8545,-0.21702c1.53744,-0.11336 3.18988,-0.24626 4.98645,-0.24626l0,0c1.79174,0 3.43926,0.1329 4.97676,0.24626l0,0l2.85442,0.21702l5.5972,17.36035c2.68278,0.74832 5.28296,1.7042 7.77458,2.86422l0,0l15.48913,-9.73007l2.32635,1.67193c2.67629,1.93114 5.24559,4.0533 7.63685,6.37647l0,0l2.04603,1.99589l-6.95963,17.11728c1.51638,2.20318 2.88363,4.53609 4.05171,6.98881l0,0l18.24804,2.5272l0.70955,2.77344c0.79706,3.12353 1.45159,6.37646 1.7836,9.78183l0,0l0.28843,2.84148l-16.41906,8.68001c-0.2462,2.69264 -0.68362,5.34303 -1.33651,7.92526l0,0l12.37055,13.66671l-1.24414,2.57896c-1.42238,2.95176 -3.02464,5.87103 -4.92169,8.63802l0,0l-1.6103,2.35868l-18.06977,-3.87169c-1.9149,1.91478 -3.9885,3.67742 -6.20477,5.26831l0,0l0.68849,18.35816l-2.61308,1.18265c-3.01005,1.35434 -6.13185,2.49161 -9.35075,3.3956l0,0l-2.75251,0.7776l-11.17653,-14.36972l0,0l0.00001,0.00001zm14.34048,4.59775c0.96397,-0.32087 1.91167,-0.66764 2.84959,-1.03701l0,0l-0.64475,-17.33748l2.03317,-1.30584c3.04562,-1.97631 5.81909,-4.34815 8.28158,-7.04052l0,0l1.63945,-1.79492l17.09785,3.66112c0.51356,-0.85866 1.01086,-1.73668 1.49036,-2.63408l0,0l-11.69496,-12.9311l0.72577,-2.31024c1.03191,-3.32753 1.61839,-6.89491 1.7917,-10.6175l0,0l0.10693,-2.42374l15.53443,-8.21019c-0.16197,-0.99476 -0.34819,-1.98294 -0.56048,-2.97451l0,0l-17.2841,-2.39097l-0.92981,-2.23574c-1.39004,-3.34387 -3.2288,-6.47368 -5.42393,-9.36054l0,0l-1.45157,-1.92462l6.57409,-16.17107c-0.76144,-0.6676 -1.5423,-1.3155 -2.34419,-1.94406l0,0l-14.67419,9.21151l-2.16116,-1.14066c-3.20112,-1.68481 -6.66318,-2.96465 -10.31475,-3.79403l0,0l-2.36197,-0.54103l-5.29909,-16.43363c-0.51523,-0.02583 -1.01579,-0.03892 -1.50342,-0.03892l0,0c-0.49739,0 -0.99794,0.01309 -1.51639,0.03892l0,0l-5.26178,16.32675l-2.39606,0.51193c-3.71477,0.79692 -7.25772,2.09627 -10.55772,3.80376l0,0l-2.15631,1.121l-14.43609,-9.0656c-0.79704,0.62856 -1.57956,1.27645 -2.33442,1.94406l0,0l6.39904,15.74667l-1.50331,1.9277c-2.34421,3.01001 -4.29314,6.30848 -5.75441,9.84985l0,0l-0.92501,2.24537l-16.72846,2.31334c-0.2122,0.99157 -0.40008,1.97975 -0.55723,2.97451l0,0l14.99663,7.92838l0.0923,2.44288c0.15711,3.94333 0.8165,7.72771 1.95048,11.26586l0,0l0.74206,2.32625l-11.32572,12.5294c0.47634,0.90067 0.97204,1.77868 1.49051,2.63729l0,0l16.63115,-3.58025l1.65572,1.79498c2.55153,2.79308 5.46596,5.2198 8.6769,7.23522l0,0l2.07205,1.29261l-0.63347,17.07187c0.93963,0.36938 1.89057,0.71614 2.85448,1.03701l0,0l10.527,-13.53701l2.40409,0.29471c1.81444,0.23009 3.58026,0.37913 5.33803,0.37913l0,0c1.89055,0 3.79729,-0.16517 5.74623,-0.44061l0,0l2.42522,-0.3401l10.60794,13.64388l0,0l0,0.00001z"
                 stroke-width="1.5" stroke="#000" fill="#fff">
                 <animateTransform attributeType="XML" attributeName="transform" begin="0s" dur="5s" type="rotate"
                     from="0 500 200" to="360 500 200" repeatCount="indefinite" />
             </path>
-            <rect id="svg_7" height="34" width="117" y="175.45313" x="267" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <rect id="svg_8" height="34" width="117" y="241.45313" x="185" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <rect id="svg_9" height="34" width="117" y="100" x="599" stroke-width="1.5" stroke="#000" fill="#fff" />
-            <rect id="svg_10" height="34" width="117" y="171.45313" x="601" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <rect id="svg_11" height="34" width="117" y="296.45313" x="498.99999" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <path id="svg_cdn"
-                d="m847.81544,124.29119c-10.22201,0 -18.51255,-7.73856 -18.51255,-17.28l0,0c0.00617,-7.65792 5.35013,-14.10912 12.73355,-16.36992l0,0c0.25609,-6.5088 5.97647,-11.70432 13.00815,-11.71008l0,0c2.25236,0 4.36588,0.5472 6.2017,1.48896l0,0c3.90615,-6.0624 11.01497,-10.12608 19.15432,-10.12896l0,0c12.17817,0.00576 22.08239,9.0864 22.36316,20.38752l0,0c7.28777,2.31552 12.533,8.73792 12.53917,16.33248l0,0c0,9.54144 -8.29054,17.28 -18.51255,17.28l0,0l-48.97496,0l0,0zm51.22115,-28.584c-1.57665,-0.2736 -2.66581,-1.61856 -2.51462,-3.10752l0,0c0.05245,-0.49248 0.08331,-0.96768 0.08331,-1.43136l0,0c-0.0216,-8.34912 -7.25692,-15.1056 -16.20157,-15.11712l0,0c-6.93912,-0.00576 -12.84154,4.08384 -15.15252,9.8208l0,0c-0.37642,0.93024 -1.25268,1.62432 -2.3079,1.8144l0,0c-1.04904,0.19008 -2.14129,-0.14976 -2.86019,-0.88992l0,0c-1.28354,-1.30464 -3.04223,-2.10528 -5.0385,-2.10528l0,0c-3.77965,0.00576 -6.8373,2.85984 -6.84964,6.38784l0,0c0,0.39168 0.05245,0.79488 0.13884,1.22112l0,0c0.15119,0.76608 -0.02468,1.5552 -0.50292,2.19456l0,0c-0.47824,0.64224 -1.20949,1.06848 -2.02712,1.19232l0,0c-5.86231,0.90144 -10.34235,5.61312 -10.33617,11.32128l0,0c0.01234,6.36192 5.52908,11.5056 12.34479,11.52288l0,0l48.97804,0c6.8157,-0.01728 12.32319,-5.16096 12.33862,-11.52288l0,0c0.01234,-5.63616 -4.35045,-10.3104 -10.09243,-11.30112l0,0"
-                stroke-width="1.5" stroke="#000" fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_13" y="123.45313" x="280" stroke-width="0" stroke="#000" fill="#000000">rtmpplugin</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_14" y="198.45313" x="282" stroke-width="0" stroke="#000" fill="#000000">rtspplugin</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_15" y="265.45313" x="205" stroke-width="0" stroke="#000" fill="#000000">hlsplugin</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_16" y="123" x="620" stroke-width="0" stroke="#000" fill="#000000">hdlplugin</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_17" y="194" x="605" stroke-width="0" stroke="#000" fill="#000000">jessicaplugin</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_18" y="320" x="506.99999" stroke-width="0" stroke="#000" fill="#000000">recordplugin</text>
-            <rect stroke="#000" id="svg_19" height="34" width="93" y="240.45313" x="334" stroke-width="1.5"
-                fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_20" y="264.45313" x="349" stroke-width="0" stroke="#000" fill="#000000">tsplugin</text>
-            <rect stroke="#000" id="svg_25" height="48" width="120.00001" y="82.45313" x="38" stroke-width="1.5"
-                fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_26" y="112.45313" x="70" stroke-width="0" stroke="#000" fill="#000000">{{$t('g.pusher')}}</text>
-            <line id="svg_27" y2="115.45313" x2="258.00001" y1="106.45313" x1="165" stroke-width="7" stroke="#000"
-                fill="none" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_29" y="127.45313" x="443.00003" stroke-width="0" stroke="#000"
-                fill="#000000">192.168.1.100</text>
-            <line stroke="#000" id="svg_30" y2="108" x2="818.00007" y1="118.45313" x1="730.00007" stroke-width="7"
-                fill="none" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_31" y="108.45313" x="852.00006" stroke-width="0" stroke="#000" fill="#000000">CDN</text>
-            <rect stroke="#000" id="svg_32" height="34" width="144.00001" y="237.45313" x="599" stroke-width="1.5"
-                fill="#fff" />
-            <text style="cursor: move;" xml:space="preserve" text-anchor="start"
-                font-family="Helvetica, Arial, sans-serif" font-size="19" id="svg_33" y="260.00001" x="603"
-                stroke-width="0" stroke="#000" fill="#000000">gatewayaplugin</text>
-            <rect id="svg_34" height="53" width="118.00001" y="161.45314" x="802.00005" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_35" y="194.45314" x="833.00006" stroke-width="0" stroke="#000"
-                fill="#000000">{{$t('g.player')}}</text>
-            <rect id="svg_36" height="52" width="121.00001" y="237.45314" x="798.00005" stroke-width="1.5" stroke="#000"
-                fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_37" y="270.45315" x="832.00006" stroke-width="0" stroke="#000"
-                fill="#000000">{{$t('g.dashboard')}}</text>
-            <rect stroke="#000" id="svg_38" height="48" width="120.00001" y="161.45314" x="37" stroke-width="1.5"
-                fill="#fff" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_39" y="193.45314" x="50" stroke-width="0" stroke="#000" fill="#000000">{{$t('g.camera')}}</text>
-            <line stroke="#000" id="svg_40" y2="190.45314" x2="258.00001" y1="184.45314" x1="164" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_41" y2="190" x2="795.00007" y1="186.45314" x1="724.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_42" y2="165.99999" x2="445.00003" y1="117.45313" x1="390.00003" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_43" y2="266.00002" x2="793.00006" y1="260.45316" x1="750.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_44" y2="122" x2="591.00005" y1="167.45314" x1="554.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_45" y2="194" x2="594.00006" y1="194.45314" x1="572.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_46" y2="257" x2="589.00005" y1="245.45314" x1="557.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_47" y2="191" x2="423.00003" y1="190.45314" x1="393.00003" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_48" y2="258.00003" x2="175.00001" y1="257.45316" x1="125.00001" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_49" y2="257.00002" x2="330.00003" y1="259.45316" x1="309.00003" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_50" y2="243.00002" x2="458.00005" y1="255.45315" x1="438.00003" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_51" y2="291.00002" x2="549.00006" y1="260.45316" x1="535.00006" stroke-width="7"
-                fill="none" />
-            <line stroke="#000" id="svg_52" y2="366" x2="648.00017" y1="342.45316" x1="616.00006" stroke-width="7"
-                fill="none" />
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_53" y="252.45314" x="27.99999" stroke-width="0" stroke="#000"
-                fill="#000000">http://****.m3u8</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="15"
-                id="svg_54" y="87.45313" x="242.00001" stroke-width="0" stroke="#000"
-                fill="#000000">rtmp://192.168.1.100/live/stream1</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="12"
-                id="svg_55" y="151.45314" x="39.99999" stroke-width="0" stroke="#000"
-                fill="#000000">rtsp://192.168.1.101/channel1</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="19"
-                id="svg_56" y="169.45314" x="273.00001" stroke-width="0" stroke="#000"
-                fill="#000000">live/stream2</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="12"
-                id="svg_57" y="78.45313" x="608.00004" stroke-width="0" stroke="#000"
-                fill="#000000">http://192.168.1.100:2020/live/stream1.flv</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="12"
-                id="svg_58" y="161.45314" x="607.00004" stroke-width="0" stroke="#000"
-                fill="#000000">ws://192.168.1.100:8080/live/stream1</text>
-            <text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="12"
-                id="svg_59" y="231.45315" x="607.00004" stroke-width="0" stroke="#000"
-                fill="#000000">http://192.168.1.100:8081</text>
         </g>
     </svg>
 </template>
@@ -139,9 +43,54 @@
 // const single = require("toml!../assets/single.toml")
 // console.log(single)
 export default {
-    data(){
+    data() {
         return {
-            
+            lineheight: 60,
+            publishers: [
+                [{ label: "Pusher" }, { label: "🧩rtmp" }],
+                [{ label: "m3u8" }, { label: "🧩hls" }, { label: "🧩ts" }]
+            ].map(x => x.map(y => ((y.bbox = {}), y)))
+        };
+    },
+    mounted() {
+        for (var i = 0; i < this.publishers.length; i++) {
+            for (var j = 0; j < this.publishers[i].length; j++) {
+                this.publishers[i][j].bbox = this.$refs[
+                    j + "t" + i
+                ][0].getBBox();
+            }
+        }
+    },
+    computed: {
+        yOffset() {
+            return (
+                200 -
+                (this.publishers.length * this.lineheight) / 2 +
+                (this.lineheight - 33) / 2
+            );
+        }
+    },
+    methods: {
+        getAfterWidth(i, j) {
+            var result = -50;
+            for (var k = j; k < this.publishers[i].length; k++) {
+                result += this.publishers[i][k].bbox.width + 50;
+            }
+            return result;
+        },
+        getPublisherAfterWidth(i, j) {
+            return 370 - this.getAfterWidth(i, j);
+        },
+        getPath(i) {
+            var h = 200 - (this.yOffset + i * this.lineheight + 15);
+            return (
+                "m400," +
+                (this.yOffset + i * this.lineheight + 15) +
+                "c50,0 0," +
+                h +
+                " 50," +
+                h
+            );
         }
     }
 };
