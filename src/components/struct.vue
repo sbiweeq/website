aa4f2c1f106e0d78c49c6943793cb347074153a3
dexter <dexter@monibuca.com>
2020-05-31 10:21:15 +0800

    新增webrtc

--- src/components/struct.vue
+++ src/components/struct.vue
@@ -64,6 +64,8 @@
 <script>
 const publishers = [
     [{ label: "Pusher" }, { label: "rtmp" }],
+    [{ label: "Pusher" }, { label: "rtsp" }],
+    [{ label: "chrome"}, { label: "webrtc"}]
     [{ label: "camera" ,pull:true}, { label: "rtsp",pull:true }],
     [{ label: "m3u8",pull:true }, { label: "hls",pull:true }, { label: "ts",pull:true }],
     [{ label: "flv files" ,pull:true}, { label: "record",pull:true }],
